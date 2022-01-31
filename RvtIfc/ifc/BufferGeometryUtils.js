79515ba3d9fb4da29c12f6ae0485069f8e5d6ea8
Emanuel Favreau <emanuel74@yahoo.com>
2022-01-31 09:31:37 -0500

    Update BufferGeometryUtils.js

--- RvtIfc/ifc/BufferGeometryUtils.js
+++ RvtIfc/ifc/BufferGeometryUtils.js
@@ -8,7 +8,7 @@ import {
 	TriangleStripDrawMode,
 	TrianglesDrawMode,
 	Vector3
-} from "https://threejs.org/build/three.module.js";
+} from "https://cdn.jsdelivr.net/npm/three@0.127.0/build/three.module.js";
 //from 'three';
 
 
